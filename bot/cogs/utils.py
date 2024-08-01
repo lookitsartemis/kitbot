1aa141ba62ac8b3684268c546a4e53063eee63c5
Juniper Watson <lookitsartemis@gmail.com>
2024-07-31 19:35:22 -0500

    Edit utils.py

--- bot/cogs/utils.py
+++ bot/cogs/utils.py
@@ -16,6 +16,7 @@ class Utilities(commands.Cog):
     # Server command
     @nextcord.slash_command(description="Gets server information.")
     async def server(self, interaction: Interaction):
+        
         server = interaction.guild
         name = server.name
         member_count = server.member_count
@@ -81,6 +82,78 @@ class Utilities(commands.Cog):
             embed.set_thumbnail(url=icon)
 
         await interaction.response.send_message(embed=embed)
+
+    @nextcord.slash_command(description="Gets user information.")
+    async def user(self, interaction: Interaction, member: Member = None):
+        
+        if member is None:
+            
+            member = interaction.user
+            
+        mention = member.mention
+        id = member.id
+        creation = f"<t:{int(member.created_at.timestamp())}:R>"
+        join_date = f"<t:{int(member.joined_at.timestamp())}:R>"
+        roles = ", ".join([role.mention for role in member.roles[1:]])
+        top_role = member.top_role.mention
+        status = member.status
+        
+        avatar = member.avatar.url
+        
+        embed = nextcord.Embed(
+            title="User",
+            color=self.color
+        )
+        embed.add_field(
+            name="Name",
+            value=mention
+        )
+        embed.add_field(
+            name="ID",
+            value=id
+        )
+        embed.add_field(
+            name="Creation",
+            value=creation
+        )
+        embed.add_field(
+            name="Joined",
+            value=join_date
+        )
+        embed.add_field(
+            name="Top Role",
+            value=top_role
+        )
+        embed.add_field(
+            name="Status",
+            value=status
+        )
+        embed.add_field(
+            name="Roles",
+            value=roles
+        )
+        embed.set_thumbnail(url=avatar)
+        
+        await interaction.response.send_message(embed=embed)
+    
+    @nextcord.slash_command(description="Gets user's avatar.")
+    async def avatar(self, interaction: Interaction, member: Member = None):
+        
+        if member is None:
+            
+            member = interaction.user
+        
+        user = member.name    
+        avatar = member.avatar.url
+        
+        embed = nextcord.Embed(
+            title=f"@{user}'s Avatar",
+            color=self.color
+        )
+        embed.set_image(url=avatar)
+        
+        await interaction.response.send_message(embed=embed)
+        
 
 def setup(bot):
     bot.add_cog(Utilities(bot))
